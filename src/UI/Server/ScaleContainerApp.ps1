779922be2fd395f6864157dc0ebf1c74b57bdb5a
Dale Marttinen <dmarttinen@gmail.com>
2023-08-10 11:19:31 -0700

    updated container app scaling script

--- src/UI/Server/ScaleContainerApp.ps1
+++ src/UI/Server/ScaleContainerApp.ps1
@@ -1,3 +1,6 @@
+param(
+    [int]$appRecplicas = 1
+)
 
 #$azPath = "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin"
 ###################################################################
@@ -38,10 +41,8 @@ Write-Host $env:PATH -Split ';'
 ###################################################################
 
 ###################################################################
-#All of that to get to this.  This the actual revision
-#update - fetches the latest container image, creates a 
-#new revision in the container app, and sets it active.
-Write-Host "Name $container_app_name"
+#All of that to get to this.  This the actual containerapp
+#update - set the number of replicas.
 & az containerapp update --name $container_app_name --resource-group $ResourceGroupName --image $container_image
-& az containerapp update --name $container_app_name --resource-group $ResourceGroupName --min-replicas 1 --max-replicas 1
+& az containerapp update --name $container_app_name --resource-group $ResourceGroupName --min-replicas $appReplicas --max-replicas $appReplicas
 ###################################################################
