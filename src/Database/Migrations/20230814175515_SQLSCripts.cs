6144ebedd9d5f81c4fc212210fc0036b790f4fab
Dale Marttinen <dmarttinen@gmail.com>
2023-08-14 12:05:32 -0700

    added entity framework and migration

---
+++ src/Database/Migrations/20230814175515_SQLSCripts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgrammingWithPalermo.ChurchBulletin.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SQLSCripts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFiles = Directory.GetFiles(@"scripts\Update", "*.sql");
+            foreach (var file in sqlFiles)
+            {
+                var script = File.ReadAllText(file);
+                migrationBuilder.Sql(script);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
