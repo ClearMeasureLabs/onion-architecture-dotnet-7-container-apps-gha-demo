bbb437b072bd32f5ed6e7879453a54028b221f39
Dale Marttinen <dmarttinen@gmail.com>
2023-08-15 10:42:42 -0700

    Revert "updated build script to generate db model"
    This reverts commit d336749c7c5f2a61cd63b478dcb5c23792194e6c.

--- src/Database/Migrations/ChurchBulletinContextModelSnapshot.cs
+++ src/Database/Migrations/ChurchBulletinContextModelSnapshot.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProgrammingWithPalermo.ChurchBulletin.Database;
+using ProgrammingWithPalermo.ChurchBulletin.Database.Models;
 
 #nullable disable
 
